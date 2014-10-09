89bf665dd5455d65787faf9fef84933fbdeb1e33
Diego Porto <diego.cfporto@gmail.com>
2014-10-09 13:40:56 -0300

    cpusched: adicionando tempo de resposta ao objeto process

--- src/CPUSched/Process.java
+++ src/CPUSched/Process.java
@@ -5,6 +5,7 @@ public class Process {
 	private int pid;
 	private int submissionTime;
 	private int burstTime;
+	private int replyTime;
 	
 	
 	public Process (int pid, int submissionTime, int burstTime){
@@ -31,7 +32,17 @@ public class Process {
 		this.burstTime--;
 	}
 	
+	
+	public int getReplyTime(){
+		return this.replyTime;
+	}
+	
+	public void setReplyTime(int replyTime){
+		this.replyTime = replyTime;
+	}
+	
 	public String toString(){
 		return "pid[" + this.pid + "] submissionTime[" + this.submissionTime + "] burstTime[" + this.burstTime + "]";
 	}
+	
 }
