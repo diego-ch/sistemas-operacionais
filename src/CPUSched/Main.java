d6daf8bc737f672e43c423640e2184866bae6ceb
Diego Porto <diego.cfporto@gmail.com>
2015-05-17 21:25:07 -0300

    cpusched: atualizando classe main

--- src/CPUSched/Main.java
+++ src/CPUSched/Main.java
@@ -7,56 +7,43 @@
 package CPUSched;
 
 import java.util.ArrayList;
-import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.logging.*;
 
 public class Main {
 
-	private final static Logger LOGGER = Logger.getLogger(Main.class.getName());
-
-	public static void  printUsage() {
-		System.out.println( "\nusage:\tjava CPUSched.Main [options] < process-list");
-	}
-
 	public static void main(String[] args) {
-
-		LOGGER.setLevel(Level.SEVERE);
 
 		int pid=0;
 		int burstTime;
 		int submissionTime;
 
-		Scanner 	        input_list   = new Scanner(System.in);
-		ArrayList<Process>  process_list = new ArrayList<Process>();
+		ArrayList<Process>  list_fcfs = new ArrayList<Process>();
+		ArrayList<Process>  list_sjf = new ArrayList<Process>();
+		ArrayList<Process>  list_rr = new ArrayList<Process>();
 
-		LOGGER.log(Level.FINE,"cpusched: reading the process list");
+		Scanner 	        input_list   = new Scanner(System.in);
+
 		if (input_list.hasNextLine()) {
 	        while (input_list.hasNext()) {
 	            submissionTime = input_list.nextInt();
 	            burstTime = input_list.nextInt();
-	            process_list.add(new Process(pid, submissionTime, burstTime));
+				list_fcfs.add(new Process(pid, submissionTime, burstTime));
+				list_sjf.add(new Process(pid, submissionTime, burstTime));
+				list_rr.add(new Process(pid, submissionTime, burstTime));
 	            pid++;
 	        }
 			input_list.close();
 		} else {
-            LOGGER.log(Level.SEVERE, "error: empty process list");
 			System.err.println("error: empty process list");
 			input_list.close();
 			System.exit(1);
 		}
 
-		// exibe a lista de processos dada como entrada
-		for (int i = 0; i < process_list.size(); i++) {
-            LOGGER.log(Level.FINE, process_list.get(i).toString());
-        }
-
 		// executa os métodos de escalonamento
-		Schedulers.runFCFS(process_list);
-        Schedulers.runSJF(process_list);
-		Schedulers.runRR(process_list);
+		Schedulers.runFCFS(list_fcfs);
+        Schedulers.runSJF(list_sjf);
+		//Schedulers.runRR(list_rr);
 
-        LOGGER.log(Level.FINE, "cpusched: closing");
 		System.exit(0);
 	}
 
